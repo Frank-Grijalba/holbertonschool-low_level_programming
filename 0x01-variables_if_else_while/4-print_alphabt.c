47dc21d73dcecc095d1b55e303b7b1c25032082d
Frank-Grijalba <fjgrijalba@gmail.com>
2021-02-05 14:27:00 -0500

    Commit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		if (ch == 'q' || ch == 'e')
+		{
+		ch++;
+		}
+		else
+		{
+		putchar(ch);
+		ch++;
+		}
+	}
+	putchar('\n');
+	return (0);
+}
