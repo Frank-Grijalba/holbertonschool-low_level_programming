b71e8c88292738bcbf399159c448bb970b54d722
Frank-Grijalba <fjgrijalba@gmail.com>
2021-02-09 21:59:11 -0500

    Commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * add - prints the add between two numbers
+ *@x: first int
+ *@y: second int
+ * Return: the add
+ **/
+
+int add(int x, int y)
+{
+return (x + y);
+}
