34e6c5ca2777a0b2a54eea0b093b479c42cc54e5
Frank-Grijalba <fjgrijalba@gmail.com>
2021-02-11 16:02:29 -0500

    0x04-more_functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ *_isdigit - check if parameter 'c' is a number
+ *@c: is entry point
+ *Return:0 or 1
+ **/
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
