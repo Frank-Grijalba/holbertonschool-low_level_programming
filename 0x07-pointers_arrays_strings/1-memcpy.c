95884d855c019cff0bd561e8769ecef4bdf03055
Frank-Grijalba <fjgrijalba@gmail.com>
2021-02-22 18:02:05 -0500

    Commit

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -2,23 +2,20 @@
 /**
  *_memcpy - fills memory with a constant byte
  *@dest: pointer to memory
- *@src: pointer source 
+ *@src: pointer source
  *@n: number of bytes to used
  *Return: pointer to memory
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-    int i=0;
-    
-	for (i=0; i < n; i++)
+	int i = 0;
+
+	for (i = 0; i < n; i++)
 	{
-	    if (i == 5){
-	    dest[i] = src [i + 1];    
-	    }
-	    else
-	    {
-		dest[i] = src [i];
-	    }
+		if (i == 5)
+			dest[i] = src[i + 1];
+		else
+			dest[i] = src[i];
 	}
 	return (dest);
 }
