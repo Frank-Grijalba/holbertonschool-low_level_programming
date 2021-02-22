523c8cc139cd20cacdb7dd38ba21c1a4b24508de
Frank-Grijalba <fjgrijalba@gmail.com>
2021-02-21 23:06:14 -0500

    Commit

--- 0x06-pointers_arrays_strings/101-magic.c
+++ 0x06-pointers_arrays_strings/101-magic.c
@@ -13,28 +13,10 @@ int main(void)
  * - you are not allowed to use a
  * - you are not allowed to modify p
  * - only one statement
-<<<<<<< HEAD
-<<<<<<< HEAD
  * - you are not allowed to code anything else than this line of code
  */
 	*(&n + 3) = 98;
 	/* ...so that this prints 98\n */
 	printf("a[2] = %d\n", a[2]);
 	return (0);
-=======
- * - you are not allowed to code anything else than line of code 
- */
-  *(p + 5) = 98;
- /* ... so that this prints 98\n */
-  printf("a[2] = %d\n", a[2]);
-  return (0);
->>>>>>> aa600c5f82d9a061efa4445f0a85405fde2c27dd
-=======
- * - you are not allowed to code anything else than this line of code
- */
-	*(&n + 3) = 98;
-	/* ...so that this prints 98\n */
-	printf("a[2] = %d\n", a[2]);
-	return (0);
->>>>>>> d412466967a859dec0d565509b9302baeea5e348
 }
