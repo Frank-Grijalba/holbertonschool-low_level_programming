1903ae69a068e96c0fcfb067668bbf839a6ed4da
Frank-Grijalba <fjgrijalba@gmail.com>
2021-02-22 17:55:02 -0500

    Commit

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ *_memcpy - fills memory with a constant byte
+ *@dest: pointer to memory
+ *@src: pointer source 
+ *@n: number of bytes to used
+ *Return: pointer to memory
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+    int i=0;
+    
+	for (i=0; i < n; i++)
+	{
+	    if (i == 5){
+	    dest[i] = src [i + 1];    
+	    }
+	    else
+	    {
+		dest[i] = src [i];
+	    }
+	}
+	return (dest);
+}
