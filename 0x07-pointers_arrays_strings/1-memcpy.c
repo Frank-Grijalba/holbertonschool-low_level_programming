b3657d69bc39785af9d8908ec8473ee8b5efc147
Frank-Grijalba <fjgrijalba@gmail.com>
2021-02-22 18:08:37 -0500

    Commit

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -12,10 +12,7 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 
 	for (i = 0; i < n; i++)
 	{
-		if (i == 5)
-			dest[i] = src[i + 1];
-		else
-			dest[i] = src[i];
+		dest[i] = src[i];
 	}
-	return (dest);
+return (dest);
 }
