7f0025b41f4a431a672eb0528cc725b94a6156cb
Frank-Grijalba <fjgrijalba@gmail.com>
2021-02-22 22:11:21 -0500

    Commit

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,37 +1,27 @@
 #include "holberton.h"
-
 /**
- * _strstr - compares two strings
- * @haystack: source string
- * @needle: string being compared
- * Description: checks if needle is at start of haystack
- * Return: 1 if matches, 0 if not
- **/
-
+ * *_strstr - Locates a substring
+ *
+ * @haystack: char string
+ * @needle: char string
+ *
+ * Return: char pointer or null
+ */
 char *_strstr(char *haystack, char *needle)
 {
-	int a = 0, b = 0, cont = 0;
+	int i;
+	int j;
 
-	while (needle[a] != '\0')
+	for (i = 0; needle[i] != '\0'; i++)
 	{
-		a++;
-	}
-	while (haystack[b] != '\0')
-	{
-		for (c = 0; c < a; a++)
+		for (j = 0; haystack[j] != '\0'; j++)
 		{
-			if (haystack[b] == needle[c])
+			if (needle[i] == haystack[j])
 			{
-				cont++;
-				if (cont == a)
-				{
-					b = b - a;
-					return (b + a);
-			}
-			else
-			{
-				cont = 0;
+				return (&haystack[j]);
 			}
 		}
 	}
+	return ('\0');
 }
+
--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -6,9 +6,7 @@
  */
 void print_chessboard(char (*a)[8])
 {
-
 	int x, y;
-
 	for (y = 0; y < 8; y++)
 	{
 		for (x = 0; x <= 8; x++)
