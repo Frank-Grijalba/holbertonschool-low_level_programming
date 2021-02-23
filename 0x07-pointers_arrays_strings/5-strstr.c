3e845fceeaaca17e854ee65bede052c548b9fc88
Frank-Grijalba <fjgrijalba@gmail.com>
2021-02-22 23:45:34 -0500

    Commit

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -2,28 +2,50 @@
 #include <stddef.h>
 
 /**
- * *_strstr - Locates a substring
+ * compare - sdfdsf
+ * @haystack: source string
+ * @needle: string being compared
+ * 
  *
- * @haystack: char string
- * @needle: char string
- *
- * Return: char pointer or null
- */
-char *_strstr(char *haystack, char *needle)
+ * Return: 1 if matches, 0 if not
+ **/
+
+int compare(char *haystack, char *needle)
 {
 	int i;
-	int j;
 
 	for (i = 0; needle[i] != '\0'; i++)
 	{
-		for (j = 0; haystack[j] != '\0'; j++)
+		if (haystack[i] == '\0')
+			return (0);
+		if (haystack[i] != needle[i])
+			return (0);
+	}
+	return (1);
+}
+
+/**
+ * _strstr - sdfdsfds
+ * @haystack: source string
+ * @needle: substring
+ * 
+ * Return: pointer to first instance, NULL if not in haystack
+ **/
+
+char *_strstr(char *haystack, char *needle)
+{
+	int j;
+
+	if (needle[0] == '\0')
+		return (haystack);
+
+	for (j = 0; haystack[j] != '\0'; j++)
+	{
+		if (needle[0] == haystack[j])
 		{
-			if (needle[i] == haystack[j])
-			{
+			if (compare(&haystack[j], needle) == 1)
 				return (&haystack[j]);
-			}
 		}
 	}
 	return (NULL);
 }
-
