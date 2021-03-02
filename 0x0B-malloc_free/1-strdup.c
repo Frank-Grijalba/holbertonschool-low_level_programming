e0b4df654253fbbd714679feb3b3e4f6d5435fa3
Frank-Grijalba <fjgrijalba@gmail.com>
2021-03-02 16:00:54 -0500

    _strdup - create a copy of string

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ *_strdup - create a copy of string
+ *@str: string to copy
+ *Return: pointer to Array, NULL if value is 0
+ */
+char *_strdup(char *str)
+{
+	char *chr;
+	int n;
+	int length = sizeof(str);
+
+	chr = malloc(sizeof(char) * length);
+
+	if (chr == NULL)
+	{
+		return (NULL);
+	}
+
+	else
+	{
+		for (n = 0; n <= length; n++)
+		{
+			chr[n] = str[n];
+		}
+	}
+	return (chr);
+}
