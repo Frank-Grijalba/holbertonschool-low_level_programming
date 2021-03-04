6ab2e17ba520c6c9a66b28064c0f3c1291efaa74
Frank-Grijalba <fjgrijalba@gmail.com>
2021-03-04 14:57:41 -0500

    Commit

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *malloc_checked - allocates memory blocks using malloc
+ *@b: mem required
+ *Return: pointer to allocated memory
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = (void *)malloc(b);
+
+	if (b <= 0)
+		exit(98);
+
+	if (ptr == NULL)
+		exit(98);
+
+	return (ptr);
+}
