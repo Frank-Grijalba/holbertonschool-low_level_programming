d98e098870e6bc05150c5df3d2c86478db30ac01
Frank-Grijalba <fjgrijalba@gmail.com>
2021-03-04 22:27:53 -0500

    Commit

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -6,45 +6,46 @@
  *                 the first n bytes of s2, and null terminated
  *@s1: string 1
  *@s2: string 2
- *@n: number of bytes that will takes of the second string and then append
- *    to the first string
+ *@n: number of bytes that will takes of the second string 
  *Return: char pointer
  */
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-
-	char *str;
-	unsigned int long1 = 0;
-	unsigned int long2 = 0;
-	unsigned int i;
+	char *ptr;
+	unsigned int size1 = 0, size2 = 0, i;
 
 	if (s1 == NULL)
-	s1 = "";
+		s1 = "";
 
 	if (s2 == NULL)
-	s2 = "";
+		s2 = "";
 
-	while (s1[long1] != '\0')
-		long1++;
+	while (s1[size1] != '\0')
+	{	size1++;
+	}
 
-	while (s1[long2] != '\0')
-		long2++;
+	while (s2[size2] != '\0')
+	{	size2++;
+	}
 
-	if (n >= long2)
-		str = malloc(sizeof(char) * (long1 + long2) + 1);
-	else
-		str = malloc(sizeof(char) * (long1 + n) + 1);
+	if (n > size2)
+		n = size2;
+	ptr = malloc((size1 + n + 1) * sizeof(char));
 
-	if (str == NULL)
+	if (ptr == NULL)
 		return (0);
 
-	for (i = 0; i < long1; i++)
-	{    str[i] = s1[i];
+	for (i = 0; i < size1; i++)
+	{
+		ptr[i] = s1[i];
 	}
-	for (; i < (long1 + n); i++)
-	{    str[i] = s2[i - long1];
+
+	for (; i < (size1 + n); i++)
+	{
+		ptr[i] = s2[i - size1];
 	}
-	str[i] = '\0';
-	return (str);
+	ptr[i] = '\0';
+
+	return (ptr);
 }
