3992e6dac4f0fd24325350ef5d9185c4801d450a
Frank-Grijalba <fjgrijalba@gmail.com>
2021-03-09 17:48:30 -0500

    Commit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -18,7 +18,11 @@ float age;
 char *owner;
 };
 
+typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+int _putchar(char c);
+dog_t *new_dog(char *name, float age, char *owner);
 
 #endif
