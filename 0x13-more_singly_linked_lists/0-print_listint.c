9a766f5f924a08886a258a217e1fbb8157018535
FRANK-GRIJALBA <fjgrijalba@gmail.com>
2021-03-23 16:40:10 -0500

    Commit

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -11,13 +11,13 @@ size_t print_listint(const listint_t *h)
 {
 	size_t count = 0;
 
+	if (h == NULL)
+	return (0);
+
 	for (count = 0; h != NULL; count++)
 	{
-		if (h != NULL)
-		{
-			printf("%d\n", h->n);
-			h = h->next;
-		}
+		printf("%d\n", h->n);
+		h = h->next;
 	}
 	return (count);
 }
