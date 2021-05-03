d167754d56647cd097df97f8cf02e528085ab93d
FRANK-GRIJALBA <fjgrijalba@gmail.com>
2021-05-03 12:40:41 -0500

    Update

--- 0x0C-more_malloc_free/1-main.c
+++ 0x0C-more_malloc_free/1-main.c
@@ -10,9 +10,13 @@
 int main(void)
 {
     char *concat;
+    char *concat2;
 
     concat = string_nconcat("Holberton ", "School !!!", 6);
-    printf("%s\n", concat);
+    concat2 = string_nconcat(concat, "Is the best", 10);
+    printf("%s\n", concat2);
+    
     free(concat);
+    free(concat2);
     return (0);
 }
