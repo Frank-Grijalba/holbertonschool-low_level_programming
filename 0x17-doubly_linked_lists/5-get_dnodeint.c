754c76ba419ab947990992ee200fa848a94ba6e9
FRANK-GRIJALBA <fjgrijalba@gmail.com>
2021-05-02 22:20:05 -0500

    Mandatory

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -15,13 +15,12 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	if (!head)
 	return (NULL);
 
-	if (temp)
+	while (temp != NULL)
 	{
-		for (i = 0; i < index; i++)
-		{
-			temp = temp->next;
-		}
-		return (temp);
+		if (i == index)
+			return (temp);
+		temp = temp->next;
+		i++;
 	}
 	return (NULL);
 }
